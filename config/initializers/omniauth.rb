3caedea3568d8f9457583a5aeaa5108a8f759cab
Jeff <herlitzj@gmail.com>
2016-01-09 12:50:30 -0500

    changed omniauth scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET"],
-    scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google'
+    image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google'
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
