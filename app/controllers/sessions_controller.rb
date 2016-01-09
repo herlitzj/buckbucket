97d30d563fa936be40f6d476aea019ae0c88197c
Jeff <herlitzj@gmail.com>
2016-01-09 12:25:20 -0500

    Rollback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,14 @@
 class SessionsController < ApplicationController
     def create
         begin
-            auth_hash = request.env['omniauth.auth']
-            @user = User.find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+            @user = User.from_omniauth(request.env['omniauth.auth'])
             session[:user_id] = @user.id
             flash[:success] = "Welcome, #{@user.name}!"
         rescue
             flash[:warning] = "There was an error while trying to authenticate you..."
         end
-        # redirect_to root_path
-        render text: request.env['omniauth.auth'].to_yaml
+        redirect_to root_path
+        # render text: request.env['omniauth.auth'].to_yaml
     end
 
     def destroy
