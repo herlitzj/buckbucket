a6c7a27c2e952ef0f022866f5c720445dbd79edd
Jeff <herlitzj@gmail.com>
2016-01-09 12:16:45 -0500

    Moved User create to controller for test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
 class SessionsController < ApplicationController
     def create
         begin
-            @user = User.from_omniauth(request.env['omniauth.auth'])
+            auth_hash = request.env['omniauth.auth']
+            @user = User.find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
             session[:user_id] = @user.id
             flash[:success] = "Welcome, #{@user.name}!"
         rescue
