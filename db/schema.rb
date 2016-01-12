710025346db08a116f2edc5f0a6af089fa2e7261
Jeff <herlitzj@gmail.com>
2016-01-12 10:22:22 -0500

    Added columns and additional venmo info to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109204315) do
+ActiveRecord::Schema.define(version: 20160112151825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,15 +37,18 @@ ActiveRecord::Schema.define(version: 20160109204315) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "name"
-    t.string   "uid",        null: false
-    t.string   "provider",   null: false
+    t.string   "uid",                 null: false
+    t.string   "provider",            null: false
     t.string   "location"
     t.string   "email"
     t.string   "url"
     t.string   "nickname"
+    t.string   "phone"
+    t.string   "venmo_token"
+    t.string   "venmo_refresh_token"
   end
 
   add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
