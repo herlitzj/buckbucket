e804e22b3c23254539f1d4fe7692ef4747cd1136
Jeff <herlitzj@gmail.com>
2016-01-16 10:00:10 -0500

    Association test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find_by(params[:user_id])
+    @markers = @user.markers
   end
 
   # GET /users/new
