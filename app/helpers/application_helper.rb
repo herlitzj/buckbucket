70f0ebe8d0946edf97ed4aad091d6d80160039b6
Jeff <herlitzj@gmail.com>
2016-01-17 11:27:10 -0500

    formatted time on marker list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+    #change time zone from server to be in EST and proper display
+    def format_time time
+        time.to_formatted_s(:long)
+    end 
+
+    helper_method :format_time
 end
--- app/helpers/markers_helper.rb
+++ app/helpers/markers_helper.rb
@@ -3,9 +3,4 @@ module MarkersHelper
     def get_user_name user_id
         User.find(user_id).name
     end
-
-    #change time zone from server to be in EST and proper display
-    def format_time time
-        time.to_formatted_s(:long)
-    end 
 end
